a5c80226b438026796f01ec2a48a5a138facf340
dattt <trantiendat20012000@gmail.com>
2024-02-20 10:04:21 +0700

    update

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -22,8 +22,8 @@ if [[ $(echo "$MIN_GO_VERSION $GO_VERSION" | tr " " "\n" | sort -V | head -n 1) 
 fi
 
 
-PRYSM_DIR=./dependencies1/prysm
-GETH_DIR=./dependencies1/go-ethereum
+PRYSM_DIR=./dependencies/prysm
+GETH_DIR=./dependencies/go-ethereum
 
 ( cd $PRYSM_DIR && go build -o=./out/beacon-chain ./cmd/beacon-chain && go build -o=./out/validator ./cmd/validator && go build -o=./out/prysmctl ./cmd/prysmctl )
 
